99d6a16fbaccbf85cc1ea8652192291b466e301e
HongTao <hongtao@iie.ac.cn>
2019-11-05 17:24:12 +0800

    todo list 2 completed

--- src/main/java/com/example/tdd/Dollar.java
+++ src/main/java/com/example/tdd/Dollar.java
@@ -5,11 +5,11 @@ public class Dollar {
     int amount;
 
     Dollar(int amount) {
-
+        this.amount = amount;
     }
 
     void times(int multiplier) {
-
+        amount *= multiplier;
     }
 
 }
