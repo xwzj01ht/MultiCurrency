79922d28d0d2908842061472cffc6a799268d0c0
HongTao <hongtao@iie.ac.cn>
2019-11-05 17:35:24 +0800

    todo list 4 completed

--- src/test/java/com/example/tdd/TestMultiCurrency.java
+++ src/test/java/com/example/tdd/TestMultiCurrency.java
@@ -6,12 +6,24 @@ import static org.junit.Assert.assertEquals;
 
 public class TestMultiCurrency {
 
-    //TODO 5美元*2=10美元
-    @Test
+    // 5美元*2=10美元
+   /* @Test
     public void testMultiplication() {
         Dollar five = new Dollar(5);
         five.times(2);
         assertEquals(10, five.amount);
+        five.times(3);
+        assertEquals(15, five.amount);
+    }*/
+
+    //TODO Dollar类有副作用吗？
+    @Test
+    public void testMultiplication() {
+        Dollar five = new Dollar(5);
+        Dollar product = five.times(2);
+        assertEquals(10, product.amount);
+        product = five.times(3);
+        assertEquals(15, product.amount);
     }
 
 }
