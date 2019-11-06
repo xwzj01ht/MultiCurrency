c41b43d422e417d5dd7fd13c12acd74ddd3dfde7
HongTao <hongtao@iie.ac.cn>
2019-11-06 14:41:46 +0800

    工厂方法接触测试程序与子类的耦合

--- src/main/java/com/example/tdd/Dollar.java
+++ src/main/java/com/example/tdd/Dollar.java
@@ -1,14 +1,12 @@
 package com.example.tdd;
 
-import java.util.Objects;
-
 public class Dollar extends Money {
 
     Dollar(int amount) {
         this.amount = amount;
     }
 
-    Dollar times(int multiplier) {
+    Money times(int multiplier) {
         return new Dollar(amount * multiplier);
     }
 
--- src/test/java/com/example/tdd/TestMultiCurrency.java
+++ src/test/java/com/example/tdd/TestMultiCurrency.java
@@ -26,28 +26,45 @@ public class TestMultiCurrency {
         assertEquals(15, product.amount);*/
 
         // 将 amount 定义为私有
-        Dollar five = new Dollar(5);
+        /*Dollar five = new Dollar(5);
         assertEquals(new Dollar(10), five.times(2));
-        assertEquals(new Dollar(15), five.times(3));
+        assertEquals(new Dollar(15), five.times(3));*/
+
+        //TODO 美元Dollar/瑞士法郎Franc之间的重复设计
+        /*Money five = Money.dollar(5);
+        assertEquals(new Dollar(10), five.times(2));
+        assertEquals(new Dollar(15), five.times(3));*/
+
+        Money five = Money.dollar(5);
+        assertEquals(Money.dollar(10), five.times(2));
+        assertEquals(Money.dollar(15), five.times(3));
     }
 
     // 实现equals()函数，使得Dollar成为数值对象
     @Test
     public void testEquality() {
+
         assertTrue(new Dollar(5).equals(new Dollar(5)));
         assertFalse(new Dollar(5).equals(new Dollar(6)));
+
+        assertTrue(Money.dollar(5).equals(Money.dollar(5)));
+        assertFalse(Money.dollar(5).equals(Money.dollar(6)));
+
         assertTrue(new Franc(5).equals(new Franc(5)));
         assertFalse(new Franc(5).equals(new Franc(6)));
 
+        assertTrue(Money.franc(5).equals(Money.franc(5)));
+        assertFalse(Money.franc(5).equals(Money.franc(6)));
+
         // 比较法郎对象和美元对象
-        assertFalse(new Franc(5).equals(new Dollar(5)));
+        assertFalse(Money.franc(5).equals(Money.dollar(5)));
     }
 
     // 5瑞士法郎*2=10瑞士法郎
     @Test
     public void testFrancMultiplication() {
-        Franc five = new Franc(5);
-        assertEquals(new Franc(10), five.times(2));
-        assertEquals(new Franc(15), five.times(3));
+        Money five = Money.franc(5);
+        assertEquals(Money.franc(10), five.times(2));
+        assertEquals(Money.franc(15), five.times(3));
     }
 }
